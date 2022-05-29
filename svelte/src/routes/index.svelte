2b2affbfdde71adf2e221fe8a71e5b4fe217cc4e
Spahr Michael <spahr.michael@protonmail.com>
2022-05-29 13:32:48 +0200

    add WalletConnect.svelte

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import WalletConnect from '$lib/WalletConnect.svelte';
+	export let web3Props = {
+		provider: null,
+		signer: null,
+		account: null,
+		chainId: null,
+		contract: null
+	};
+</script>
+
+<h1>My Tamagotchi</h1>
+{#if !web3Props.account}
+	<WalletConnect bind:web3Props />
+{:else}
+	<h2>?</h2>
+{/if}
